8df41abd21ade6ce46da0a8ec50fa9c681964488
Justin Saborouh <rubikmanjustin@gmail.com>
2022-09-01 15:25:19 -0700

    added to lists

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+    const task = document.getElementById('new-task-description').value;
+    console.log(task);
+    const newTask = document.createElement('li');
+
+    newTask.textContent = task;
+    document.getElementById('tasks').appendChild(newTask);
+    let delBtn = document.createElement("button")
+    delBtn.textContent = 'Delete';
+    newTask.appendChild(delBtn);
+    delBtn.addEventListener('click', function(event){
+      document.getElementById('tasks').removeChild(newTask);
+      
+    })
+  });
+
+  /*function addList(){
+    const task = document.getElementbyId('new-task-description');
+    const newTask = document.createElement('li');
+    newTask.textContent = task;
+    document.getElementById('tasks').appendChild(newTask);
+  }*/
 });
